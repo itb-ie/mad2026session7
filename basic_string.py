a430cb0ae28f2930a790e011c8b0029247f6e786
Bogdan Ratiu <bogdan.ratiu@velocitaconsultancy.uk>
2026-01-30 11:48:11 +0100

    first string example

---
+++ basic_string.py
@@ -0,0 +1,18 @@
+
+text = "Hello World"
+print(text)
+text = 'Hello World 2' # single vs double
+print(text[4])
+print(len(text)) # length of the text
+text = ""
+print(len(text))
+
+text = "Bob"
+print(text[-1]) # last character in the string
+print(text[-3])
+
+# print(text[3]) # this is an error
+print(text[6//3])
+
+
+
